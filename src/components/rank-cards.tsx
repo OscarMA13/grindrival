a89f762e896030998482cb8ea3ddbd783b610600
OscarMA13 <omartinez5676@gmail.com>
2025-08-03 23:42:12 -0700

    feat: update rankCards component to improve layout and add description display

--- src/components/rank-cards.tsx
+++ src/components/rank-cards.tsx
@@ -1,32 +1,38 @@
 import { Button } from './ui/button'
 import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card'
 
-export default function rankCards({ name, rank, description, weight }: { name: string; rank: string; description: string; weight: string }) {
+export default function rankCards({ name, rank, description, weight }: { name: string; rank?: string; description: string; weight: string }) {
     return (
         <Card className="h-full w-full">
             <CardHeader>
-                <CardTitle>{name} Rank</CardTitle>
-                <CardDescription>{description}</CardDescription>
+                <CardTitle className="text-xl font-semibold">{name} Rank</CardTitle>
+                <CardDescription>
+                    <p>current weight: {weight} kg</p>
+                </CardDescription>
                 <CardAction></CardAction>
             </CardHeader>
             <CardContent>
-                <div className="flex w-full flex-row items-center justify-between gap-4">
-                    <p>current weight: {weight} kg</p>
-                    <img
-                        src={
-                            rank === 'bronze'
-                                ? '/images/bronze.png'
-                                : rank === 'silver'
-                                  ? '/images/silver.png'
-                                  : rank === 'gold'
-                                    ? '/images/gold.png'
-                                    : rank === 'platinum'
-                                      ? '/images/plat.png'
-                                      : '/images/default.png'
-                        }
-                        alt={`${name} image`}
-                        className="h-30 w-20"
-                    />
+                <div className="flex w-full flex-col items-center justify-between gap-4">
+                    {rank && (
+                        <>
+                            <img
+                                src={
+                                    rank === 'bronze'
+                                        ? '/images/bronze.png'
+                                        : rank === 'silver'
+                                          ? '/images/silver.png'
+                                          : rank === 'gold'
+                                            ? '/images/gold.png'
+                                            : rank === 'platinum'
+                                              ? '/images/plat.png'
+                                              : '/images/default.png'
+                                }
+                                alt={`${name} image`}
+                                className="h-30 w-20"
+                            />
+                            <p>{description}</p>
+                        </>
+                    )}
                 </div>
             </CardContent>
             <CardFooter>
