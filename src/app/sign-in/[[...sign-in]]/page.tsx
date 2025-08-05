b2d792e63e5bc68b0a6417126837ac973a67f7f1
OscarMA13 <121266982+OscarMA13@users.noreply.github.com>
2025-08-05 12:53:57 -0700

    feat: add fallback redirect URL to SignIn and SignUp components

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs'
 export default function Page() {
     return (
         <div className="flex h-screen items-center justify-center bg-gray-900">
-            <SignIn forceRedirectUrl={process.env.CLERK_SIGN_IN_FORCE_REDIRECT_URL} />
+            <SignIn forceRedirectUrl={process.env.CLERK_SIGN_IN_FORCE_REDIRECT_URL} fallbackRedirectUrl={process.env.CLERK_SIGN_IN_FALLBACK_REDIRECT_URL} />
         </div>
     )
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs'
 export default function Page() {
     return (
         <div className="flex h-screen items-center justify-center bg-gray-900">
-            <SignUp forceRedirectUrl={process.env.CLERK_SIGN_UP_FORCE_REDIRECT_URL} />
+            <SignUp forceRedirectUrl={process.env.CLERK_SIGN_UP_FORCE_REDIRECT_URL} fallbackRedirectUrl={process.env.CLERK_SIGN_UP_FALLBACK_REDIRECT_URL} />
         </div>
     )
 }
